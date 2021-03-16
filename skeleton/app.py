f31b62817f3306ec927afbfc05a43e12ea95d475
Emily Greven <65984633+egreven@users.noreply.github.com>
2021-03-16 16:32:47 -0400

    Update app.py
    Deleted SQL password info

--- skeleton/app.py
+++ skeleton/app.py
@@ -23,7 +23,7 @@ app.secret_key = 'super secret string'  # Change this!
 
 #These will need to be changed according to your creditionals
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Penelope1595'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'PASSWORD'
 app.config['MYSQL_DATABASE_DB'] = 'photoshare'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
